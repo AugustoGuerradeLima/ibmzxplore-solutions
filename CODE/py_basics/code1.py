986c6609b43249f445a778d54003bde8bdb427fe
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-10-19 14:01:48 -0300

    CODE

--- CODE/py_basics/code1.py
+++ CODE/py_basics/code1.py
@@ -8,23 +8,15 @@ my_number = 5
 
 while (my_number > 0):
    
-   #set the variable "now" to the current time
    now = datetime.now()
    
-   #format the variable "the_time" so it shows the hour, minute, and seconds of the current time
    the_time = now.strftime("%H:%M:%S")
    
-   #print out the current time in the format of "the_time"
-   #Notice how we combined literal text ("The current time is") with "the_time"
-   #by sticking a plus sign between them like that. 
    print("The current time is " + the_time)
    
-   #subtract 1 from "my_number"
    my_number = my_number - 1
-   
-   #Take a 1-second nap
-   time.sleep(1)
 
+   time.sleep(1)
 
 your_userid = getpass.getuser()
 
